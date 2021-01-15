1303ade8dabe832ac3291aa0bddd6c6b0544b46f
dzikriqalampacil <70832561+dzikriqalampacil@users.noreply.github.com>
2021-01-15 21:22:31 +0700

    Make more realistic hover

---
+++ bikinweb.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $('#campus').on('click', function(){
+        alert('yaw');
+    });
+});
+
+$(document).ready(function(){
+    $(".header p").hover(function(){
+        $(this).css("background-color", "#F5A417 ");
+        $(this).css("border-radius", "25px");
+        $(this).css("width", "auto");
+        $(this).css("height", "auto");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+})
+        
+
